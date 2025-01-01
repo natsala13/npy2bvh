9edeb8cd76d0804e3c1c8b1bf91bdbd969477f1d
nathansala <natouda@gmail.com>
2025-01-01 19:59:05 +0200

    personal info for pypi.

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='mp2bvh',
     version='0.2.4',
+    author='Nathan Sala',
+    author_email='natouda@gmail.com',
+    description='Convert between different representation of human motions.',
     packages=find_packages(include=['mp2bvh', 'mp2bvh.*']),
-
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
     entry_points={
         'console_scripts': ['mp2bvh=mp2bvh.main:main']
     }
 )
-
-
